a04b677d4592ae32b20b8760f8a5401059bbd03a
oman1219 <32309792+oman1219@users.noreply.github.com>
2021-12-05 17:27:34 -0500

    background color

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,9 +8,9 @@ type LayoutProps = {
 
 const Layout = ({children}: LayoutProps) => {
   return(
-    <div className="flex flex-col items-center justify-center  px-10 min-h-screen bg-gray-50	overflow-hidden	">
+    <div className="flex flex-col items-center justify-center  px-10 min-h-screen 	overflow-hidden	">
       <header>
-        <nav className="h-8  bg-gray-50	 absolute text-xl flex flex-row items-start justify-start mt-10 inset-0 px-10 lg:left-1/4 lg:px-2	">
+        <nav className="h-8 absolute text-xl flex flex-row items-start justify-start mt-10 inset-0 px-10 lg:left-1/4 lg:px-2	">
           <Link href={'/'}>
             <a className="font-bold ">Home</a>
           </Link>
