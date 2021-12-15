502c2d7827970f10134d35ad103fff549ed88457
oman1219 <32309792+oman1219@users.noreply.github.com>
2021-12-14 21:20:54 -0500

    keepin it functional

--- lib/metatags.ts
+++ lib/metatags.ts
@@ -1,27 +1,35 @@
-const axios = require('axios');
+const axios = require("axios");
 type OgTags = {
-   [key: string]: string;
-}
+  [key: string]: string;
+};
 
-export async function parseOG(url:string) {
-  const contents = await axios.request({ url }).then((res:any):any => {return res.data});
-  const tokens = contents.split('>').map((e:string)=>e.trim()).filter((e:string)=>e.length > 0)
-  const ogtags: OgTags ={}
-  tokens.forEach((e:string)=>{
-    if (e.charAt(0).match(/[<]/) && e.charAt(1)!='/') {
-      if (e.match(/og:/)) {
-        const tag = e.match(/"og:(\w+)"/)
-        const content = e.match(/content="(.+)"/)
-        if (tag && content) {
-            ogtags[tag[1]] = content[1]
+export async function parseOG(url: string) {
+  const contents = await axios
+    .request({ url })
+    .then((res: any): any => res.data);
+  const tokens = contents
+    .split(">")
+    .map((e: string) => e.trim())
+    .filter((e: string) => e.length > 0);
+  const ogtags: OgTags = tokens.reduce((pv, cv) => {
+    if (cv.charAt(0).match(/[<]/) && cv.charAt(1) !== "/") {
+      if (cv.match(/og:/)) {
+        const tagData = cv.match(/"og:(\w+)"/);
+        const contentData = cv.match(/content="(.+)"/);
+        if (tagData && contentData) {
+          const tag = tagData[1];
+          const content = contentData[1];
+          pv[tag] = content;
         }
       }
     }
-  })
+    return pv;
+  }, {});
   if (!ogtags.title) {
-    const fbtitle = contents.match(/<title>(.+)<\/title>/)
+    const fbtitle = contents.match(/<title>(.+)<\/title>/);
     if (fbtitle) {
-      ogtags.title = fbtitle[1]
+      const title = fbtitle[1];
+      ogtags["title"] = title;
     }
   }
   return ogtags;
